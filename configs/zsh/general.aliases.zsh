5f81a581018beb1955f433c4f9e30d4496b77b4a
Devadathan M B <devadathanmb@gmail.com>
2025-09-07 01:22:33 +0530

    updates

--- configs/zsh/general.aliases.zsh
+++ configs/zsh/general.aliases.zsh
@@ -9,6 +9,5 @@ alias down="cd ~/Downloads"
 alias mv="mv -i"
 
 # Vim Aliases
-alias lvim="nvim -u ~/.config/nvim/init.lua"
 alias vim="nvim -u ~/.config/cvim/init.lua"
 alias nvim="nvim -u ~/.config/cvim/init.lua"
